130573b7f93ea5e5ccfe7d813dfe11052f0509fc
Joanne Basa <joannegbasa@gmail.com>
2021-01-23 17:50:09 +0000

    add exercise 4

--- 4.php
+++ 4.php
@@ -17,13 +17,14 @@
 	<?php  
 
 /*  Step1: Define a function and make it return a calculation of 2 numbers
-
 	Step 2: Make a function that passes parameters and call it using parameter values
-
-
  */
+    function addNumbers($a, $b) {
+        return ($a + $b); // needs to be a variable
+    }
 
-	
+    $result = addNumbers(3, 2);
+    echo $result;
 ?>
 
 
