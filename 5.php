1fc30c31227c78b214e960cf21f38ce7457f5c83
Joanne Basa <joannegbasa@gmail.com>
2021-01-24 12:39:43 +0000

    add exercise 5

--- 5.php
+++ 5.php
@@ -15,17 +15,30 @@
 	<?php 
 
 
-/*  Step1: Use a pre-built math function here and echo it
-
-
+/*
+    Step1: Use a pre-built math function here and echo it - also called internal functions
 	Step 2:  Use a pre-built string function here and echo it
-
-
 	Step 3:  Use a pre-built Array function here and echo it
-
  */
+    // built-in math function
+    $number = 11.54376;
+    $roundedNumber = round($number); // Math.round() is in JS!!!!
 
-	
+    echo "$roundedNumber";
+    echo "<br>";
+
+    // built-in string function
+    $string = "This is a string in reverse";
+    $reversedString = strrev($string);
+
+    echo "$reversedString";
+    echo "<br>";
+
+    // built-in array function
+    $beatles = ["John", "Paul", "George", "Ringo"];
+
+    echo "The Beatles has " . sizeof($beatles) . " members";
+
 ?>
 
 
