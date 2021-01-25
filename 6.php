408608ec63811167a76c86db0d1ffd2ecfcacb53
Joanne Basa <joannegbasa@gmail.com>
2021-01-25 10:59:25 +0000

    add basic form - WIP

--- 6.php
+++ 6.php
@@ -12,17 +12,33 @@
 
 
 <article class="main-content col-xs-8">
- 
+<!--    Step1: Make a form that submits one value to POST super global-->
+    <form action="6.php" method="post">
+        Name:
+        <input type="text" name="name" value="<?php $name ="";
+        echo $name;?>"><br>
 
-	<?php  
+        E-mail: <input type="text" name="email" value="<?php $email ="";
+        echo $email;?>"><br>
 
-/*  Step1: Make a form that submits one value to POST super global
+        Website: <input type="text" name="website" value="<?php $website ="";
+        echo $website;?>"><br>
 
+        Comment: <textarea name="comment" rows="5" cols="40"><?php $comment ="";
+        echo $comment;?></textarea><br>
 
- */
+        Gender:
+        <input type="radio" name="gender"
+            <?php if (isset($gender) && $gender=="female") echo "checked";?>
+               value="female">Female
+        <input type="radio" name="gender"
+            <?php if (isset($gender) && $gender=="male") echo "checked";?>
+               value="male">Male
+        <input type="radio" name="gender"
+            <?php if (isset($gender) && $gender=="other") echo "checked";?>
+               value="other">Other
+    </form>
 
-	
-?>
 
 
 </article><!--MAIN CONTENT-->
